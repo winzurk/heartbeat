558f2b1550db3ca9b7d9487c66bf2437189de582
Zac Winzurk <33220369+winzurk@users.noreply.github.com>
2018-11-22 04:55:08 -0700

    Update heart.py

--- heart.py
+++ heart.py
@@ -123,24 +123,3 @@ def load_tensor_from_numpy(data_path, numpy_path):
 
     return X, y
 
-
-def get_train_test(split_ratio=0.6, random_state=42):
-    # Get available labels
-    labels, indices, _ = get_labels(DATA_PATH)
-
-    # Getting first arrays
-    X = np.load(labels[0] + '.npy')
-    y = np.zeros(X.shape[0])
-
-    # Append all of the dataset into one single array, same goes for y
-    for i, label in enumerate(labels[1:]):
-        x = np.load(label + '.npy')
-        X = np.vstack((X, x))
-        y = np.append(y, np.full(x.shape[0], fill_value= (i + 1)))
-
-    assert X.shape[0] == len(y)
-
-    return train_test_split(X, y, test_size= (1 - split_ratio), random_state=random_state, shuffle=True)
-
-
-
